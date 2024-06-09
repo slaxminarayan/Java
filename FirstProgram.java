224e82308aa0662834fc9d790e2201fca0594244
Laxminarayan Sharma <laxminarayannsharma@gmail.com>
2024-06-09 23:54:34 +0530

    File renamed

--- FirstProgram.java
+++ FirstProgram.java
@@ -1,4 +1,4 @@
-public class BasicProgram {
+public class FirstProgram {
     public static void main(String[] args) {
         System.out.println("Hehe I started learning/revising java, 2016-2018 Laxminarayan Sharma is back");
     }
