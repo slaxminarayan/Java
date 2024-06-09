1431312e514132a55599aa709f1a5b1ad4f9de7f
Laxminarayan Sharma <laxminarayannsharma@gmail.com>
2024-06-09 23:44:58 +0530

    First Prgram to start java, Old LS is back

---
+++ BasicProgram.java
@@ -0,0 +1,5 @@
+public class BasicProgram {
+    public static void main(String[] args) {
+        System.out.println("Hehe I started learning/revising java, 2016-2018 Laxminarayan Sharma is back");
+    }
+}
